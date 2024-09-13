6853d2dac8ad6795fd57647d0e697768a2c4c212
Bernard <cesarzb@protonmail.com>
2024-09-14 00:33:15 +0200

    Fixed allowed hosts bug

--- pomodorotracker/settings.py
+++ pomodorotracker/settings.py
@@ -21,7 +21,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS', ''].split(',')
+try:
+    ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
+except:
+    ALLOWED_HOSTS = []
+    
 
 # Application definition
 
